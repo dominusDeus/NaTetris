9619138dcd181485762a15865a543ef94067e527
nataliacarrera <104646501+nataliacarrera@users.noreply.github.com>
2024-02-28 00:07:38 -0300

    fixed holdBox styles

--- src/components/hold-box.tsx
+++ src/components/hold-box.tsx
@@ -8,13 +8,15 @@ interface HoldBoxProps {
 
 const HoldBox = ({ holdBoxPiece }: HoldBoxProps) => {
   return (
-    <div className="flex h-[160px] w-[200px] flex-col items-center gap-6 border-4 border-solid border-gray-600 bg-black">
-      <p className="py-5 text-2xl text-gray-300">HOLD</p>
-      <Piece
-        atoms={holdBoxPiece.atoms}
-        className={twMerge("scale-75")}
-        color={holdBoxPiece.color}
-      />
+    <div className=" flex h-[160px] w-[200px] flex-col items-center border-4 border-solid border-gray-600 bg-black p-2">
+      <div className="flex h-full flex-col items-center justify-between">
+        <p className="py-2 text-2xl text-gray-300">HOLD</p>
+        <Piece
+          atoms={holdBoxPiece.atoms}
+          className={twMerge("scale-[0.65]")}
+          color={holdBoxPiece.color}
+        />
+      </div>
     </div>
   )
 }
